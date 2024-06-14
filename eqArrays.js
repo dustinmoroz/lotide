30fa2168c4213b2362bd35e565c4ef9b6f038d3d
Dustin Moroz <dustin.moroz@gmail.com>
2024-06-14 14:49:34 -0600

    Updated as per feedback. Changed function to compare each element of an array to each element of another array. Added additional test cases.

--- eqArrays.js
+++ eqArrays.js
@@ -8,18 +8,24 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-
 const eqArrays = function(array1, array2) {
-  if (array1 !== array2) {
+  if (array1.length !== array2.length) {
     return false;
+  } else {
+    let result = false;
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      } else {
+        result = true;
+      }
+    }
+    return result;
   }
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]);
-eqArrays([1, 2, 3], [3, 2, 1]);
-eqArrays(["1", "2", "3"], ["1", "2", "3"]);
-eqArrays(["1", "2", "3"], ["1", "2", 3]);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([], []), true);
